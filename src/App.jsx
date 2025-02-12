b9a59fdf7f947e1a23e50f9b3e59234658da47d7
Lovensih Semwal <lovenishsemwal179@gmail.com>
2025-02-12 20:55:19 +0530

    admin dashboard created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Login from './components/Auth/Login'
 import EmplyeeDashboard from './components/Dashboard/EmplyeeDashboard'
+import AdminDashboard from './components/Dashboard/AdminDashboard'
 
 const App = () => {
   return (
     <>
     {/* <Login/> */}
-    <EmplyeeDashboard/>
+    {/* <EmplyeeDashboard/> */}
+    <AdminDashboard/>
     </>
   )
 }
--- src/components/Other/TaskListNumber.jsx
+++ src/components/Other/TaskListNumber.jsx
@@ -10,18 +10,18 @@ const TaskListNumber = () => {
       </div>
 
       <div className='rounded-xl py-6 px-9 w-[45%] bg-blue-400'>
-        <h2 className='text-3xl font-semibold'>0</h2>
-        <h3 className='text-xl font-medium'>New Task</h3>
+        <h2 className='text-3xl font-semibold'>1</h2>
+        <h3 className='text-xl font-medium'>To be done</h3>
       </div>
 
       <div className='rounded-xl py-6 px-9 w-[45%] bg-green-400'>
-        <h2 className='text-3xl font-semibold'>0</h2>
-        <h3 className='text-xl font-medium'>New Task</h3>
+        <h2 className='text-3xl font-semibold'>2</h2>
+        <h3 className='text-xl font-medium'>Completed</h3>
       </div>
 
       <div className='rounded-xl py-6 px-9 w-[45%] bg-yellow-400'>
-        <h2 className='text-3xl font-semibold'>0</h2>
-        <h3 className='text-xl font-medium'>New Task</h3>
+        <h2 className='text-3xl font-semibold'>3</h2>
+        <h3 className='text-xl font-medium'>Completed</h3>
       </div>
 
     </div>
